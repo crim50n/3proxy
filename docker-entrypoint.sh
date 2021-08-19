947cea70ee5b297d068289942f431c97683cbbb8
crim50n <crims0n@yandex.ru>
2021-08-20 00:32:19 +0300

    update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "start" ]; then
-    if [ ! -f /etc/3proxy.cfg ]; then
+    if [ ! -f /etc/3proxy/3proxy.cfg ]; then
         if [ -z "$PROXY_LOGIN" ] || [ -z "$PROXY_PASSWORD" ]; then
             echo >&2 'error: proxy is uninitialized, variables is not specified '
             echo >&2 '  You need to specify PROXY_LOGIN and PROXY_PASSWORD'
@@ -15,34 +15,34 @@ if [ "$1" = "start" ]; then
 
         trap _term SIGTERM
 
-        echo "writable" > /etc/3proxy.cfg
-        echo "nserver 1.1.1.1" >> /etc/3proxy.cfg
-        echo "nserver 8.8.8.8" >> /etc/3proxy.cfg
-        echo "nserver 8.8.4.4" >> /etc/3proxy.cfg
-        echo "nscache 65536" >> /etc/3proxy.cfg
-        echo "nsrecord www.porno.com 127.0.0.1" >> /etc/3proxy.cfg
-        echo "" >> /etc/3proxy.cfg
-        echo 'counter "/etc/3proxy/cfg/3proxy.3cf" D "/etc/3proxy/cfg/traf"' >> /etc/3proxy.cfg
-        echo "log" >> /etc/3proxy.cfg
-        echo 'logformat "L%t%. L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"' >> /etc/3proxy.cfg
-        echo "" >> /etc/3proxy.cfg
-        echo "monitor /etc/3proxy.cfg" >> /etc/3proxy.cfg
-        echo "" >> /etc/3proxy.cfg
-        echo "users $PROXY_LOGIN:CL:$PROXY_PASSWORD" >> /etc/3proxy.cfg
-        echo "" >> /etc/3proxy.cfg
-        echo "auth strong" >> /etc/3proxy.cfg
-        echo "proxy -p3128" >> /etc/3proxy.cfg
-        echo "socks -p1080" >> /etc/3proxy.cfg
-        echo "admin -p8080" >> /etc/3proxy.cfg
-        echo "" >> /etc/3proxy.cfg
-        echo "flush" >> /etc/3proxy.cfg
+        echo "writable" > /etc/3proxy/3proxy.cfg
+        echo "nserver 1.1.1.1" >> /etc/3proxy/3proxy.cfg
+        echo "nserver 8.8.8.8" >> /etc/3proxy/3proxy.cfg
+        echo "nserver 8.8.4.4" >> /etc/3proxy/3proxy.cfg
+        echo "nscache 65536" >> /etc/3proxy/3proxy.cfg
+        echo "nsrecord www.porno.com 127.0.0.1" >> /etc/3proxy/3proxy.cfg
+        echo "" >> /etc/3proxy/3proxy.cfg
+        echo 'counter "/etc/3proxy/3proxy.3cf" D "/etc/3proxy/traf"' >> /etc/3proxy/3proxy.cfg
+        echo "log" >> /etc/3proxy/3proxy.cfg
+        echo 'logformat "L%t%. L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"' >> /etc/3proxy/3proxy.cfg
+        echo "" >> /etc/3proxy/3proxy.cfg
+        echo "monitor /etc/3proxy/3proxy.cfg" >> /etc/3proxy/3proxy.cfg
+        echo "" >> /etc/3proxy/3proxy.cfg
+        echo "users $PROXY_LOGIN:CL:$PROXY_PASSWORD" >> /etc/3proxy/3proxy.cfg
+        echo "" >> /etc/3proxy/3proxy.cfg
+        echo "auth strong" >> /etc/3proxy/3proxy.cfg
+        echo "proxy -p3128" >> /etc/3proxy/3proxy.cfg
+        echo "socks -p1080" >> /etc/3proxy/3proxy.cfg
+        echo "admin -p8080" >> /etc/3proxy/3proxy.cfg
+        echo "" >> /etc/3proxy/3proxy.cfg
+        echo "flush" >> /etc/3proxy/3proxy.cfg
 
         echo "Proxy user login:    $PROXY_LOGIN"
         echo "Proxy user password: $PROXY_PASSWORD"
         echo "Proxy process started!"
     fi
 
-    /usr/local/bin/3proxy /etc/3proxy.cfg &
+    /usr/local/bin/3proxy /etc/3proxy/3proxy.cfg &
 
     child=$!
     wait "$child"
